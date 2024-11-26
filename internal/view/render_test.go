4add012f7b288d6b6bcf72295734c863ee6f021d
Bruno Schaatsbergen <git@bschaatsbergen.com>
2024-11-26 11:52:03 +0100

    view: add NewJSONRenderer and NewHumanRenderer tests

--- internal/view/render_test.go
+++ internal/view/render_test.go
@@ -21,6 +21,19 @@ func TestNewRenderer_human(t *testing.T) {
 	assert.Equal(t, &b, hv.(*HumanRenderer).view.Stream.Writer)
 }
 
+// TestNewHumanRenderer should simply return a HumanRenderer.
+func TestNewHumanRenderer(t *testing.T) {
+	b := bytes.Buffer{}
+	hv := NewView(&b)
+	hr := NewHumanRenderer(hv)
+
+	// Check that the view is a HumanRenderer
+	assert.IsType(t, &HumanRenderer{}, hr)
+
+	// Check that the view's stream writer is the same as the buffer
+	assert.Equal(t, &b, hr.view.Stream.Writer)
+}
+
 // TestNewRenderer_JSON tests the NewRenderer function, which should return a JSONRenderer
 // and bind provided io.Writer to the view's stream writer.
 func TestNewRenderer_JSON(t *testing.T) {
@@ -32,4 +45,17 @@ func TestNewRenderer_JSON(t *testing.T) {
 
 	// Check that the view's stream writer is the same as the buffer
 	assert.Equal(t, &b, jv.(*JSONRenderer).view.Stream.Writer)
+}
+
+// TestNewJSONRenderer should simply return a JSONRenderer.
+func TestNewJSONRenderer(t *testing.T) {
+	b := bytes.Buffer{}
+	jv := NewJSONView(NewView(&b))
+	jr := NewJSONRenderer(jv)
+
+	// Check that the view is a JSONRenderer
+	assert.IsType(t, &JSONRenderer{}, jr)
+
+	// Check that the view's stream writer is the same as the buffer
+	assert.Equal(t, &b, jr.view.Stream.Writer)
 }
